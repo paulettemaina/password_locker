4e8990124f88c13861189a36a5e10b6ba829e4ef
Paulette Maina <paulettemaina@gmail.com>
2018-06-10 22:16:14 +0300

    Add save multiple users test

--- test.py
+++ test.py
@@ -39,7 +39,15 @@ class TestUser(unittest.TestCase):
         self.new_user.save_user() # saving the new user
         self.assertEqual(len(User.user_list),1)
 
-    
+    def test_save_multiple_user(self):
+        '''
+        test-save_multiple_user to check if we can save multiple 
+        user objects to our user_list
+        '''
+        self.new_user.save_user()
+        test_user = User("Ace","Ventura","1234") #new Contact
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
 
 
 
