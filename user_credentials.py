2dcf8ca152d38d4ee8501337dba1a28135d48b16
Paulette Maina <paulettemaina@gmail.com>
2018-06-10 22:13:32 +0300

    Add save user function code

--- user_credentials.py
+++ user_credentials.py
@@ -10,4 +10,14 @@ class User:
         self.last_name = last_name
         self.password = password
 
-    
+    def save_user(self):
+        '''
+        save_user method saves user object into user_list
+        '''
+
+        User.user_list.append(self)
+
+
+
+
+
