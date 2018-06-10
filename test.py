307ae6dcbd990659b70bbf95dadee1b230232851
Paulette Maina <paulettemaina@gmail.com>
2018-06-10 22:30:26 +0300

    Create test for finding user via login_name

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ class TestUser(unittest.TestCase):
         '''
         Setup method to run before each test case
         '''
-        self.new_user = User("Ace","Ventura","1234")
+        self.new_user = User("Ace","1234")
 
     def tearDown(self):
         '''
@@ -26,8 +26,7 @@ class TestUser(unittest.TestCase):
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.new_user.first_name,"Ace")
-        self.assertEqual(self.new_user.last_name,"Ventura")
+        self.assertEqual(self.new_user.login_name,"Ace")
         self.assertEqual(self.new_user.password,"1234")
 
 
@@ -45,9 +44,22 @@ class TestUser(unittest.TestCase):
         user objects to our user_list
         '''
         self.new_user.save_user()
-        test_user = User("Ace","Ventura","1234") #new Contact
+        test_user = User("Ace","1234") #new User
         test_user.save_user()
         self.assertEqual(len(User.user_list),2)
+
+    def test_find_user_by_login_name(self):
+        '''
+        test to check if we can find a contact by phone number and display information
+        '''
+
+        self.new_user.save_user()
+        test_user = User("Ace","1234") #new user
+        test_user.save_user()
+
+        found_user = User.find_by_login_name("Ace")
+        self.assertEqual(found_user.login_name,test_user.login_name)
+
 
 
 
