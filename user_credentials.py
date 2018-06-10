c02cb0d45be128fe9fb2f4b53ca353ae10d29019
Paulette Maina <paulettemaina@gmail.com>
2018-06-10 22:12:31 +0300

    Add initialising method

---
+++ user_credentials.py
@@ -0,0 +1,13 @@
+import pyperclip
+class User:
+    '''
+    Class that generates new instances of users
+    '''
+    user_list = [] # Empty user list
+
+    def __init__(self,first_name,last_name,password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+    
