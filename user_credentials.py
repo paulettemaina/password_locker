01634380d6ee3356ffccdbff3a86d59e9aadc66d
Paulette Maina <paulettemaina@gmail.com>
2018-06-10 22:31:21 +0300

    Add function for finding user via their login name

--- user_credentials.py
+++ user_credentials.py
@@ -5,9 +5,8 @@ class User:
     '''
     user_list = [] # Empty user list
 
-    def __init__(self,first_name,last_name,password):
-        self.first_name = first_name
-        self.last_name = last_name
+    def __init__(self,login_name,password):
+        self.login_name = login_name
         self.password = password
 
     def save_user(self):
@@ -17,7 +16,33 @@ class User:
 
         User.user_list.append(self)
 
+    @classmethod
+    def find_by_login_name(cls,login_name):
+        '''
+        Method that takes in a login_name and returns a user that matched that login_name.
+
+        Args:
+            login_name: Phone login_name to search for
+        Returns:
+             Contact of person that matched the login_name.
+        '''
+
+        for user in cls.user_list:
+            if user.login_name == login_name:
+                return user
 
 
 
 
+
+class Credentials:
+    '''
+    Class that generates new instances of credentials
+    '''
+    Credentials_list = []
+
+    def __init__(self,website_name,user_name,pass_key):
+        self.website_name = website_name
+        self.user_name = user_name
+        self.pass_key = pass_key
+    
